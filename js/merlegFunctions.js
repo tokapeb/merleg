b78830e6d25452da81e203f98b4712be2a761010
tokapeb <tokap@gmail.com>
2017-02-04 14:09:13 +0100

    Add scroll and gotoTop functions.

--- js/merlegFunctions.js
+++ js/merlegFunctions.js
@@ -1,4 +1,23 @@
 // Add automatic subject.
 $('a[data-toggle="modal"]').mousedown(function() {
-  $('.modal-body #subject').attr("placeholder", $(this).data("subject"));
+//  $('.modal-body #subject').attr("placeholder", $(this).data("subject"));
+  $('.modal-body #subject').attr("value", $(this).data("subject"));
+});
+
+$("a[href='#top']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
+
+$('#topbutton').hide();
+
+var entryheight = $('.highlighted').height();
+
+$(document).scroll(function () {
+  var y = $(this).scrollTop();
+  if (y > entryheight) {
+    $('#topbutton').fadeIn();
+  } else {
+    $('#topbutton').fadeOut();
+  }
 });
